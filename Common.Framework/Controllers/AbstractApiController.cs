46e47f1681cea7a4de8a6d97f67d88afe0aa9897
karen yeow <karenyeow188@me.com>
2018-02-20 12:07:49 +1100

    Squashed 'comlib/' changes from e765b23..251f6b7
    251f6b7 ...
    
    git-subtree-dir: comlib
    git-subtree-split: 251f6b77d5c93abdec4da5e413a953f6f26a6418

---
+++ Common.Framework/Controllers/AbstractApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Comlib.Common.Helpers.Email;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iCare.Api.Controllers
+{
+    public class AbstractApiController : Controller
+    {
+        private readonly IOneGovEmailSender _oneGovEmailSender;
+
+        public AbstractApiController(IOneGovEmailSender oneGovEmailSender)
+        {
+            _oneGovEmailSender = oneGovEmailSender;
+        }
+
+        protected async Task<IActionResult> HandleException(Exception ex)
+        {
+            await _oneGovEmailSender.SendErrorEmailAsync(ex);
+            return new BadRequestObjectResult(new
+            {
+                Error = ex.HResult.ToString(),
+                ErrorDescription = ex.Message
+            });
+        }
+    }
+}
